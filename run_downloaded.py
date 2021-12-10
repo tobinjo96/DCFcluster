7feda7be265ce1f3c6a172f0164325e752ffd183
Joshua Tobin <69868231+tobinjo96@users.noreply.github.com>
2021-12-10 16:48:51 +0000

    Update run_downloaded.py

--- run_downloaded.py
+++ run_downloaded.py
@@ -5,13 +5,18 @@ import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 import warnings
 
+from sklearn.preprocessing import StandardScaler
 from sklearn import cluster, datasets, metrics
 import csv
 
 Data = np.load("Dermatology.npy")
-X = Data[:, range(X.shape[1] - 1)]
-y = Data[:, X.shape[1] - 1]
 # normalize dataset for easier parameter selection
+X = Data[:, range(Data.shape[1] - 1)]
+y = Data[:, Data.shape[1] - 1]
+nonans = np.isnan(X).sum(1) == 0
+X = X[nonans,:]
+y = y[nonans]
+X = StandardScaler().fit_transform(X)
 result = DCFcluster.train(X, k = 20, beta = 0.4)
 ami = metrics.adjusted_mutual_info_score(y.astype(int), result.labels.astype(int))
 #ARI
@@ -21,9 +26,13 @@ with open("DCF_Results.csv", 'a') as fd:
     writer.writerow(["DCF", "Dermatology",  k, beta, len(np.unique(result.labels)), ari,ami])
 
 Data = np.load("Ecoli.npy")
-X = Data[:, range(X.shape[1] - 1)]
-y = Data[:, X.shape[1] - 1]
 # normalize dataset for easier parameter selection
+X = Data[:, range(Data.shape[1] - 1)]
+y = Data[:, Data.shape[1] - 1]
+nonans = np.isnan(X).sum(1) == 0
+X = X[nonans,:]
+y = y[nonans]
+X = StandardScaler().fit_transform(X)
 result = DCFcluster.train(X, k = 8, beta = 0.4)
 ami = metrics.adjusted_mutual_info_score(y.astype(int), result.labels.astype(int))
 #ARI
@@ -33,9 +42,13 @@ with open("DCF_Results.csv", 'a') as fd:
     writer.writerow(["DCF", "Ecoli",  k, beta, len(np.unique(result.labels)), ari,ami])
 
 Data = np.load("Glass.npy")
-X = Data[:, range(X.shape[1] - 1)]
-y = Data[:, X.shape[1] - 1]
 # normalize dataset for easier parameter selection
+X = Data[:, range(Data.shape[1] - 1)]
+y = Data[:, Data.shape[1] - 1]
+nonans = np.isnan(X).sum(1) == 0
+X = X[nonans,:]
+y = y[nonans]
+X = StandardScaler().fit_transform(X)
 result = DCFcluster.train(X, k = 16, beta = 0.4)
 ami = metrics.adjusted_mutual_info_score(y.astype(int), result.labels.astype(int))
 #ARI
@@ -45,9 +58,13 @@ with open("DCF_Results.csv", 'a') as fd:
     writer.writerow(["DCF", "Glass",  k, beta, len(np.unique(result.labels)), ari,ami])
 
 Data = np.load("Letter-Recognition.npy")
-X = Data[:, range(X.shape[1] - 1)]
-y = Data[:, X.shape[1] - 1]
 # normalize dataset for easier parameter selection
+X = Data[:, range(Data.shape[1] - 1)]
+y = Data[:, Data.shape[1] - 1]
+nonans = np.isnan(X).sum(1) == 0
+X = X[nonans,:]
+y = y[nonans]
+X = StandardScaler().fit_transform(X)
 result = DCFcluster.train(X, k = 20, beta = 0.4)
 ami = metrics.adjusted_mutual_info_score(y.astype(int), result.labels.astype(int))
 #ARI
